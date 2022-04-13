a71a11db18ab0f169db57f77ee7bd99f10f9b4dc
Aleksei Chirkov <91471671+Lexiety@users.noreply.github.com>
2022-04-13 14:17:43 +0500

    Update 9Labcode.cpp

--- 9Lab/9Labcode.cpp
+++ 9Lab/9Labcode.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int pos1, pos2;
     
-    string tmp, dmp,aaa;
+    string tmp, dmp,aaa,bbb;
     bool fl=false;
     fstream F1("F1.txt", ios::in);
     fstream F2("F2.txt", ios::out);
@@ -17,17 +17,55 @@ int main()
         dmp = tmp;
         while (dmp.length() > 1) {
             pos1 = dmp.find(" ");
-            cout << pos1 << endl;
             aaa.assign(dmp, 0, pos1);
-            cout << aaa << endl;
-            pos2= dmp.rfind(aaa);
-            cout << pos2 << endl;
+            pos2 = dmp.rfind(aaa);
             if (0 != pos2) { fl = true; break; }
             dmp.erase(0, pos1 + 1);
         }
-        if (fl==true) { F2 << tmp << endl; }
-        
+        if (fl == true) { F2 << tmp << endl; }
     }
+
+
+    F1.clear();
+    F1.seekg(0, ios::beg);
+
+    int count = 0;
+    int maxc = 0;
+    int imax = 0;
+    fl = false;
+    string answer;
+    while (getline(F1, tmp))
+    {
+        dmp = tmp;
+        cout << dmp << endl;
+        int count = 0;
+        int i = 0;
+        fl = false;
+
+        while (dmp.length() != (i + 1))
+        {
+            if ((dmp[i] == 'a') or (dmp[i] == 'A')) { count++; }
+            if ((dmp[i] == ' ') && (maxc < count)) { maxc = count; count = 0; imax = i; fl = true; }
+            if ((dmp[i] == ' ') && (maxc > count)) {count = 0; }
+            i++;
+
+           
+        }
+
+        if (fl = true) 
+        {
+            pos1 = imax;
+            pos2 = dmp.rfind(" ", pos1-1);
+            cout << pos2 << endl;
+            cout << pos1 << endl;
+
+            answer.assign(dmp, pos2+1, pos1-pos2-1);
+        }
+
+
+    }
+
+    F2 << answer << endl;
     F1.close();
     F2.close();
     return 0;
